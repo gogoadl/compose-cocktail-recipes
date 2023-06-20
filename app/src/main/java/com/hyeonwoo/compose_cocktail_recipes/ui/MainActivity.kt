3ef038aead2104a0218f82eaef0b7a3c6e75f28e
HyeonWoo Park <49335446+gogoadl@users.noreply.github.com>
2023-06-20 19:10:21 +0900

    feat : 메인 화면 앱바 작업 중

--- app/src/main/java/com/hyeonwoo/compose_cocktail_recipes/ui/MainActivity.kt
+++ app/src/main/java/com/hyeonwoo/compose_cocktail_recipes/ui/MainActivity.kt
@@ -15,11 +15,21 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.currentCompositionLocalContext
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
@@ -28,10 +38,12 @@ import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.hyeonwoo.compose_cocktail_recipes.R
 import com.hyeonwoo.compose_cocktail_recipes.ui.theme.ComposecocktailrecipesTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -41,13 +53,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposecocktailrecipesTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    MainScreen()
-                }
+                Main()
             }
         }
     }
--- app/src/main/java/com/hyeonwoo/compose_cocktail_recipes/ui/MainScreen.kt
+++ app/src/main/java/com/hyeonwoo/compose_cocktail_recipes/ui/MainScreen.kt
@@ -19,12 +19,26 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
@@ -35,6 +49,7 @@ import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -57,6 +72,40 @@ import com.hyeonwoo.compose_cocktail_recipes.ui.theme.ComposecocktailrecipesThem
 import kotlinx.coroutines.flow.flatMapLatest
 import java.lang.StrictMath.min
 
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Main() {
+    var expanded by remember { mutableStateOf(false) }
+    var textState by remember { mutableStateOf("") }
+    Scaffold(topBar = {
+        TopAppBar(
+            title = { Text(text = stringResource(R.string.app_name))},
+            actions = {
+                IconButton(onClick = { expanded = !expanded }) {
+                    if (expanded)
+                    TextField(
+                        value = textState,
+                        onValueChange = { },
+                    )
+                    Icon(
+                        contentDescription = "Search",
+                        imageVector = Icons.Default.Search,
+                        modifier = Modifier.padding(end = 10.dp),
+                    )
+                }
+            })
+    }) {
+        Surface(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(it),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            MainScreen()
+        }
+    }
+}
 
 @Composable
 fun MainScreen(
@@ -203,10 +252,51 @@ sealed class NavScreen(val route: String) {
     }
 }
 
-@Preview(showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun DefaultPreview() {
-    ComposecocktailrecipesTheme {
-        MainScreen()
+fun MainPreview() {
+    var expanded by remember { mutableStateOf(false) }
+    var textState by remember { mutableStateOf("") }
+    Scaffold(topBar = {
+        TopAppBar(
+            title = { Text(text = stringResource(R.string.app_name))},
+            actions = {
+                IconButton(onClick = { expanded = !expanded }) {
+                    if (expanded)
+                        TextField(
+                            value = textState,
+                            onValueChange = { },
+                        )
+                    Icon(
+                        contentDescription = "Search",
+                        imageVector = Icons.Default.Search,
+                        modifier = Modifier.padding(end = 10.dp),
+                    )
+                }
+            })
+    }) {
+        Surface(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(it),
+            color = MaterialTheme.colorScheme.background
+        ) {
+        }
     }
 }
+@Preview(showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+fun DefaultMainPreview() {
+    ComposecocktailrecipesTheme {
+        MainPreview()
+    }
+}
+
+//@Preview(showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
+//@Composable
+//fun DefaultPreview() {
+//    ComposecocktailrecipesTheme {
+//        MainScreen()
+//    }
+//}
