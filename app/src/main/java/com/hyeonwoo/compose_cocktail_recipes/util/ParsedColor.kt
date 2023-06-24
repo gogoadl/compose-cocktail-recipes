385e85c8e5e9753419221a82b12253ff47829d04
HyeonWoo Park <49335446+gogoadl@users.noreply.github.com>
2023-06-24 19:53:18 +0900

    fix : 색상 팔레트 기본값 수정
    + 배경색상용 색상의 기본색상을 검정색에서 흰색으로 변경

--- app/src/main/java/com/hyeonwoo/compose_cocktail_recipes/util/ParsedColor.kt
+++ app/src/main/java/com/hyeonwoo/compose_cocktail_recipes/util/ParsedColor.kt
@@ -3,10 +3,10 @@ package com.hyeonwoo.compose_cocktail_recipes.util
 import androidx.compose.ui.graphics.Color
 
 data class ParsedColor(
-    val lightVibrantSwatch: String = "#000000",
-    val vibrantSwatch: String = "#000000",
-    val lightMutedSwatch: String = "#000000",
-    val mutedSwatch: String = "#000000",
+    val lightVibrantSwatch: String = "#FFFFFF",
+    val vibrantSwatch: String = "#FFFFFF",
+    val lightMutedSwatch: String = "#FFFFFF",
+    val mutedSwatch: String = "#FFFFFF",
     val lightVibrantSwatchBody: String = "#808080",
     val vibrantSwatchBody: String = "#808080",
     val lightMutedSwatchBody: String = "#808080",
