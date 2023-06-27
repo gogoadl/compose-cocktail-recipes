6a5d82aaacaf5271aa805d2ac11d9831da5e83cb
HyeonWoo Park <49335446+gogoadl@users.noreply.github.com>
2023-06-27 14:26:57 +0900

    refactor : gradle 파일 정리
    + 불필요 라이브러리 제거
    + 코드 정리

--- app/src/main/java/com/hyeonwoo/compose_cocktail_recipes/ui/detail/CocktailDetails.kt
+++ app/src/main/java/com/hyeonwoo/compose_cocktail_recipes/ui/detail/CocktailDetails.kt
@@ -63,7 +63,6 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
