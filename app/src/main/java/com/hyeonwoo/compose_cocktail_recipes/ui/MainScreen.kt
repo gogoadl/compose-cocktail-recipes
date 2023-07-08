3ce3d2c513b09bccf1b595f0c8cef10f7055ef2d
HyeonWoo Park <gogoadl@naver.com>
2023-07-08 14:51:09 +0900

    feat : 커스텀 서치바 컴포저블 적용

--- app/src/main/java/com/hyeonwoo/compose_cocktail_recipes/ui/MainScreen.kt
+++ app/src/main/java/com/hyeonwoo/compose_cocktail_recipes/ui/MainScreen.kt
@@ -1,12 +1,6 @@
 package com.hyeonwoo.compose_cocktail_recipes.ui
 
-import LoadingAnimation
-import android.content.Context
 import android.content.res.Configuration
-import android.widget.Toast
-import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -14,63 +8,23 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.MoreVert
-import androidx.compose.material.icons.filled.Search
-import androidx.compose.material.icons.filled.Star
-import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.ListItem
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SearchBar
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.rememberBottomSheetScaffoldState
-import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.Center
-import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
-import androidx.compose.ui.draw.scale
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.semantics.isContainer
-import androidx.compose.ui.semantics.semantics
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.ui.zIndex
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavType
@@ -78,19 +32,12 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
-import coil.compose.AsyncImage
-import coil.compose.AsyncImagePainter
-import coil.compose.rememberAsyncImagePainter
-import coil.request.ImageRequest
 import com.hyeonwoo.compose_cocktail_recipes.R
 import com.hyeonwoo.compose_cocktail_recipes.model.Cocktail
 import com.hyeonwoo.compose_cocktail_recipes.model.Drink
 import com.hyeonwoo.compose_cocktail_recipes.ui.detail.CocktailDetails
-import com.hyeonwoo.compose_cocktail_recipes.ui.state.SearchState
 import com.hyeonwoo.compose_cocktail_recipes.ui.theme.ComposecocktailrecipesTheme
-import kotlinx.coroutines.flow.flatMapLatest
-import timber.log.Timber
-import java.lang.StrictMath.min
+import com.hyeonwoo.compose_cocktail_recipes.util.SearchAppBar
 
 
 @Composable
@@ -105,31 +52,30 @@ fun MainScreen(
 ) {
     val navController = rememberNavController()
     val cocktails = mainViewModel.cocktailState.collectAsState(initial = Cocktail(emptyList<Drink?>())).value
+
+    val searchText by mainViewModel.searchText.collectAsState()
     val searchState by mainViewModel.searchState.collectAsState()
+
     NavHost(navController = navController, startDestination = NavScreen.Home.route) {
         composable(
             route = NavScreen.Home.route
         ) {
             Scaffold(topBar = {
-                TopAppBar(title = { Text(text = stringResource(id = R.string.app_name) )})
+                SearchAppBar(
+                    title = stringResource(id = R.string.app_name),
+                    query = searchText,
+                    placeholder = "",
+                    targetState = searchState,
+                    onSearch = { mainViewModel.updateSearchActive(searchState.not()) },
+                    onQueryChange = { mainViewModel.updateSearchText(it) },
+                    onQueryDone = { mainViewModel.updateSearchActive(searchState.not()) } ,
+                    onClose = { mainViewModel.updateSearchText("") })
                 }
             ) {
                 Column(modifier = Modifier.padding(it)) {
-                    SearchBar(
-                        query = searchState.currentCocktailText,
-                        onQueryChange = { mainViewModel.updateSearchText(it) },
-                        onSearch = { mainViewModel.updateSearchActive(false) },
-                        active = searchState.isActive,
-                        onActiveChange = {
-                            mainViewModel.updateSearchActive(it)
-                        },
-                        placeholder = { Text(stringResource(id = R.string.search_hint)) },
-                        leadingIcon = { Icon(Icons.Default.Search, contentDescription = null) },
-                        modifier = Modifier.fillMaxWidth().padding(10.dp)
-                    ) {}
                     Cards(
                         cocktails = cocktails,
-                        searchState = searchState,
+                        searchText = searchText,
                         selectCocktail = {
                             navController.navigate("${NavScreen.CocktailDetails.route}/$it")
                     })
@@ -154,7 +100,7 @@ fun MainScreen(
 @Composable
 fun Cards(
     cocktails: Cocktail,
-    searchState: SearchState,
+    searchText: String,
     selectCocktail: (String) -> Unit
 ) {
     if (!cocktails.drinks.isNullOrEmpty()) {
@@ -202,12 +148,14 @@ fun Cards(
             }
         }
     } else {
-        Column(modifier = Modifier.fillMaxSize().padding(top = 100.dp),
+        Column(modifier = Modifier
+            .fillMaxSize()
+            .padding(top = 100.dp),
             horizontalAlignment = Alignment.CenterHorizontally) {
             Row() {
                 Text(text = stringResource(id = R.string.no_result),
                     fontSize = 20.sp)
-                Text(text = "\"${searchState.currentCocktailText}\"",
+                Text(text = "\"${searchText}\"",
                     fontWeight = FontWeight.Bold,
                     fontSize = 20.sp)
             }
--- app/src/main/java/com/hyeonwoo/compose_cocktail_recipes/ui/MainViewModel.kt
+++ app/src/main/java/com/hyeonwoo/compose_cocktail_recipes/ui/MainViewModel.kt
@@ -1,26 +1,15 @@
 package com.hyeonwoo.compose_cocktail_recipes.ui
 
-import android.util.Log
-import androidx.compose.runtime.collectAsState
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import com.hyeonwoo.compose_cocktail_recipes.model.Cocktail
-import com.hyeonwoo.compose_cocktail_recipes.model.Drink
 import com.hyeonwoo.compose_cocktail_recipes.network.repository.CocktailRepository
 import com.hyeonwoo.compose_cocktail_recipes.ui.state.SearchState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.update
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
 import timber.log.Timber
 import javax.inject.Inject
 
@@ -28,27 +17,26 @@ import javax.inject.Inject
 class MainViewModel @Inject constructor(
     private val cocktailRepository: CocktailRepository
 ): ViewModel() {
-    private val _searchState = MutableStateFlow(SearchState())
-    val searchState: StateFlow<SearchState> = _searchState.asStateFlow()
+    private val _searchText = MutableStateFlow("")
+    val searchText = _searchText.asStateFlow()
+
+    private val _searchState = MutableStateFlow(false)
+    val searchState = _searchState.asStateFlow()
 
     @OptIn(ExperimentalCoroutinesApi::class)
-    val cocktailState = searchState.flatMapLatest {
-        if (it.currentCocktailText.isEmpty())
+    val cocktailState = searchText.flatMapLatest {
+        if (it.isEmpty())
             cocktailRepository.getRandomCocktail()
         else
-            cocktailRepository.getCocktails(it.currentCocktailText)
+            cocktailRepository.getCocktails(it)
     }
 
     fun updateSearchText(searchText: String) {
         Timber.i("searchText : $searchText")
-        _searchState.update {
-            it.copy(currentCocktailText = searchText)
-        }
+        _searchText.value = searchText
     }
 
     fun updateSearchActive(isActive: Boolean) {
-        _searchState.update {
-            it.copy(isActive = isActive)
-        }
+        _searchState.value = isActive
     }
 }
